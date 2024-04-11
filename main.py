6339e45f5c34a10f5e555baf87954e2fffbc4189
Gabriel Henrique Wetzel <gabrielhwetzel@gmail.com>
2024-04-11 11:44:12 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,57 @@
+import random
+import streamlit as st
+import plotly.express as px
+
+
+def create_dice(size):
+    """Generates dice as a list"""
+    dice = [i for i in range(1, size+1)]
+    return dice
+
+
+def roll_dice(dice):
+    """Two-step randomize
+    First shuffles list then picks a random index
+    Should be enough to create an almost non-pseudo random result"""
+    random.shuffle(dice)
+    size = len(dice)
+    return dice[random.randint(0, size-1)]
+
+
+def generate_dict(size):
+    dictionary = {"values": [], "results": []}
+    if size == 1:
+        return {"values": 1, "results": 0}
+    else:
+        for i in range(1, size + 1):
+            dictionary["values"].append(i)
+            dictionary["results"].append(0)
+        return dictionary
+
+
+size_of_dice = st.number_input("Dice size", min_value=1)
+amount_of_dice = st.number_input("Dice amount", min_value=1)
+dice_rolled = 0
+
+roll = st.button("Roll")
+
+if roll:
+    all_results = generate_dict(size_of_dice)
+    while dice_rolled < amount_of_dice:
+        # Rolls dice
+        dice_to_roll = create_dice(size_of_dice)
+        dice_result = roll_dice(dice_to_roll)
+
+        # Stores result to dictionary
+        all_results["results"][dice_result-1] += 1
+
+        dice_rolled += 1
+
+    figure = px.bar(all_results,
+                    x=all_results["values"], y=all_results["results"],
+                    labels={"x": "Values", "y": "Results"})
+    st.plotly_chart(figure)
+
+
+if __name__ == "__main__":
+    print("its main")
