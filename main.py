f6ea448fafda31c170d6a405a96bf2800795c463
Gabriel Henrique Wetzel <gabrielhwetzel@gmail.com>
2024-04-17 13:30:10 -0300

    Removed unnecessary parts

--- main.py
+++ main.py
@@ -9,32 +9,17 @@ amount_of_dice = st.number_input("Dice amount", min_value=1)
 roll = st.button("Roll")
 
 if roll:
-    roller.interpreter(roll_input)
-
-    # Class because in the future there can be multi rolls of different sizes happening
-    dice = roller.MultiDice(size_of_dice, amount_of_dice)
-    result = dice.list_rolls()
+    result = roller.interpreter(roll_input)
     st.info(result)
 
 
 if __name__ == "__main__":
+    # Class because in the future there can be multi rolls of different sizes happening
+    dice = roller.MultiDice(size_of_dice, amount_of_dice)
+    result = dice.list_rolls()
     # Testing area
-    """
-    all_results = multiroll.generate_dict()
-    
-    Alternative Option on generating dictionary, not sure how to make it work thou:
-        dictionary = {}
-        for i in range(1, self.dice.size + 1):
-            print(i)
-            dictionary[i] = 0
-        print(dictionary)
-        for i in self.all_results:
-            dictionary[i] += 1
-        print(dictionary)
-        
-    # Chart
-    figure = px.bar(all_results,
-                    x=all_results["values"], y=all_results["results"],
-                    labels={"x": "Value", "y": "Results"},
-                    range_y=(0, amount_of_dice))
-    st.plotly_chart(figure)"""
+    """    # Chart
+        figure = px.bar(results, y=results["results"],
+                        labels={"x": "Value", "y": "Results"},
+                        range_y=(0, amount_of_dice))
+        st.plotly_chart(figure)"""
--- roller.py
+++ roller.py
@@ -3,33 +3,32 @@ import re
 
 
 class Dice:
-
-    def __init__(self, size):
+    def __init__(self, dice_size):
         """Generates dice as a list"""
-        self.size = size
-        self.dice = [i for i in range(1, size+1)]
+        self.dice_size = dice_size
 
     def roll(self):
-        return self.dice[random.randint(0, self.size-1)]
+        """ Singular dice roll"""
+        return random.randint(0, self.dice_size - 1)+1
 
 
 class MultiDice(Dice):
-    def __init__(self, times_to_roll, size):
-        super().__init__(size)
-        self.multi = times_to_roll
+    def __init__(self, roll_amount, dice_size):
+        super().__init__(dice_size)
+        self.roll_amount = roll_amount
         self.all_results = []
 
     def list_rolls(self):
-        for i in range(0, self.multi):
+        """Rolls a number of times and stores rolls as a list"""
+        for i in range(0, self.roll_amount):
             result = self.roll()
             self.all_results.append(result)
-        self.all_results.sort()
         return self.all_results
 
     def generate_dict(self):
         """ Generates dictionary with mapped values for graph display."""
         dictionary = {"values": [], "results": []}
-        for i in range(1, self.size + 1):
+        for i in range(1, self.dice_size + 1):
             dictionary["values"].append(i)
             dictionary["results"].append(0)
         for i in self.all_results:
@@ -41,22 +40,26 @@ def interpreter(text: str):
     # Splits dice apart
     text_list = text.split("+")
     text_list = [text.strip() for text in text_list]
-    print("Unfiltered text:", text_list)
+
+    all_results = []
 
     # Filters out the dice
     for i in text_list:
-        pattern = re.compile("([0-9]+)[dD]([0-9]+)")
         # Dice is a tuple: (Amount to roll, Dice size)
+        pattern = re.compile("([0-9]+)[dD]([0-9]+)")
         dice_set = re.findall(pattern, i)[0]
-        print(dice_set)
+
         # Changes to int. Text is not valuable as of now
         dice_set = (int(dice_set[0]), int(dice_set[1]))
+
         print(dice_set)
         multi = MultiDice(dice_set[0], dice_set[1])
-        print("Results are:", multi.list_rolls())
-    pass
+        results = multi.list_rolls()
+        print("Results are:", results)
+        all_results.append(results)
+    return all_results
 
 
 if __name__ == "__main__":
     # Testing area
-    interpreter("2d10 + asdge1D10teststsad")
+    interpreter("20d10 + asdge1D10teststsad")
