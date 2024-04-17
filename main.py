878fc08361f7df90283603e30329844cca9a66b6
Gabriel Henrique Wetzel <gabrielhwetzel@gmail.com>
2024-04-17 10:18:52 -0300

    completed Transfer

--- main.py
+++ main.py
@@ -4,33 +4,31 @@ import roller
 
 size_of_dice = st.number_input("Dice size", min_value=2)
 amount_of_dice = st.number_input("Dice amount", min_value=1)
-dice_rolled = 0
 
 roll = st.button("Roll")
 
 if roll:
     dice = roller.Dice(size_of_dice)
+    # Multi Roll Option
     if amount_of_dice > 1:
-        multiroll = roller.Multiroll()
-        all_results = multiroll.generate_dict(size_of_dice)
-        while dice_rolled < amount_of_dice:
-
-            # Rolls dice
-            dice_result = dice.roll()
-
-            # Stores results to dictionary
-            all_results["results"][dice_result-1] += 1
-
-            dice_rolled += 1
+        # Class because in the future there can be multi rolls of different sizes happening
+        multiroll = roller.Multiroll(dice, amount_of_dice)
+        result = multiroll.roll()
+        st.info(result)
+        # Chart
+        all_results = multiroll.generate_dict()
         figure = px.bar(all_results,
                         x=all_results["values"], y=all_results["results"],
                         labels={"x": "Value", "y": "Results"},
                         range_y=(0, amount_of_dice))
         st.plotly_chart(figure)
+
+    # Single Dice Option
     else:
         result = dice.roll()
         result = str(result)
         st.info(result)
 
 if __name__ == "__main__":
+    # Testing area
     print("its main")
--- roller.py
+++ roller.py
@@ -5,20 +5,39 @@ class Dice:
 
     def __init__(self, size):
         """Generates dice as a list"""
+        self.size = size
         self.dice = [i for i in range(1, size+1)]
 
     def roll(self):
-        size = len(self.dice)
-        return self.dice[random.randint(0, size-1)]
+        return self.dice[random.randint(0, self.size-1)]
 
 
 class Multiroll:
-    def __init__(self):
-        pass
+    def __init__(self, dice_to_roll, times_to_roll):
+        self.dice = dice_to_roll
+        self.multi = times_to_roll
+        self.all_results = []
 
-    def generate_dict(self, size):
+    def roll(self):
+        for i in range(0, self.multi):
+            result = self.dice.roll()
+            self.all_results.append(result)
+        self.all_results.sort()
+        return self.all_results
+
+    def generate_dict(self):
         dictionary = {"values": [], "results": []}
-        for i in range(1, size + 1):
+        for i in range(1, self.dice.size + 1):
             dictionary["values"].append(i)
             dictionary["results"].append(0)
+        for i in self.all_results:
+            dictionary["results"][i-1] += 1
         return dictionary
+
+
+if __name__ == "__main__":
+    # Testing area
+    dice = Dice(20)
+    multi = Multiroll(dice, 10)
+    multi.roll()
+    multi.generate_dict()
